027e2a0dff03e7c21adab91ade6ebe205a0593c7
lepst0928 <lepstein@chicagobooth.edu>
2022-04-12 21:18:48 +0000

    Generate likes2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: "author_id"
   has_many :follow_requests_recieved, foreign_key: "recipient"
   has_many :follow_requests_sent, foreign_key: "sender"
+  has_many :likes, foreign_key: "fan_id"
 
  end
