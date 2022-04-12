7caad27bfad53697ef1a2ac62c894625f94c5917
lepst0928 <lepstein@chicagobooth.edu>
2022-04-12 21:07:56 +0000

    Follow requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
   has_many :comments, foreign_key: "author_id"
+  has_many :follow_requests, foreign_key: "recipient"
+  has_many :follow_requests, foreign_key: "sender"
 
  end
