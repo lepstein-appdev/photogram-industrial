58f3c8cf363dfb8513345d8d2a0614227f9df937
lepst0928 <lepstein@chicagobooth.edu>
2022-04-13 19:32:47 +0000

    more scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,8 @@ class User < ApplicationRecord
 
   has_many :liked_photos, through: :likes, source: :photo
 
-  has_many :accepted_sent_follow_requests, -> { where(status: "accepted")}, foreign_key: :sender_id, class_name: "FollowRequest"
-  has_many :accepted_recieved_follow_requests, -> { where(status: "accepted")}, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_recieved_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
 
   has_many :followers, through: :accepted_recieved_follow_requests, source: :sender
