39b7a33e2225608f3695639aeabc66742097f38d
lepst0928 <lepstein@chicagobooth.edu>
2022-04-14 17:18:15 +0000

    ?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "photos#index"
 
-  #get "/users/:id" => "users#show", as: :user
   resources :users, only: :show
 
   devise_for :users
@@ -15,4 +14,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
  # root to: "home#index"
+
+ get "/:username" => "users#show"
+
 end
