3399744ea4f4d0496420fff1620d6591181642c9
lepst0928 <lepstein@chicagobooth.edu>
2022-04-19 19:12:27 +0000

    discover page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,8 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
   end
 
+  def discover
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ get ":username/liked" => "photos#liked", as: :liked_photos
 get ":username/feed" => "users#feed", as: :feed
 get ":username/followers" => "users#followers", as: :followers
 get ":username/following" => "users#following", as: :following
+get ":username/discover" => "users#discover", as: :discover
 
 get ":username" => "users#show", as: :user
 
